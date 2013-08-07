55eacfd343b24e70443a7b320d19ac7a232db875
Robert Jakob <rj@robertjakob.de>
2013-08-07 08:36:55 +0200

    Added assert_equal_list.

--- test/test.ml
+++ test/test.ml
@@ -1,8 +1,40 @@
 include OUnit
 (*open ProglangUtils*)
 
+open Batteries
+
 type tests = (string * test_fun) list
-    
+
+let assert_equal_list ?cmp ?printer exp out =
+  let c = match cmp with
+    | Some(cmp) -> cmp
+    | None -> compare
+  in
+  let exp' = BatList.sort c exp in
+  let out' = BatList.sort c out in
+  let comparison = BatList.map2 c exp' out' in
+  let txt = "Lists are not equivalent" in
+    if BatList.exists (fun i -> i != 0) comparison then
+      match printer with
+        | Some(p) ->
+            let txt_exp = String.concat "," @@ List.map p exp in
+            let txt_out = String.concat "," @@ List.map p out in
+              assert_failure @@ Printf.sprintf "%s [%s] [%s] " txt txt_exp txt_out
+        | None -> assert_failure txt
+
+(*let assert_result_equal ?cmp ?printer exp out =
+  let comparator =
+    match cmp with
+      | Some(c) -> c
+      | None -> compare
+  in
+    match exp,out with
+      | Ok(exp'),Ok(out') ->
+          comparator exp out
+      | Bad(exp'),Bad(out') ->
+      | _ -> assert_failure "Results are not equivalent" (* TODO Add printer:
+                                                          result -> string *)
+ *)
 
 let g_all_tests : (string * (unit -> tests)) list ref = ref []
   
