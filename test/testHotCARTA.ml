ab07ebe3ccdd5a0b0ca8430a8e5aa703afec0796
Robert Jakob <rj@robertjakob.de>
2013-09-27 16:35:54 +0200

    Test case for simple num carta.

--- test/testHotCARTA.ml
+++ test/testHotCARTA.ml
@@ -15,6 +15,7 @@ open HotSimple.SimpleCARTA
 
 let zero = ("zero", o)
 let nil = ("nil", o)
+let succ = ("succ", o ^=> o)
 
 (* accepts *)
 
@@ -27,6 +28,16 @@ let carta_zero =
   let rules = RuleSet.singleton r in
     create sigma states rules q
 
+(** A CARTA that accepts numbers, i.e. zero/succ lists *)
+let carta_number = 
+  let sigma = RA.of_list [zero;succ] in
+  let q = mk_single_state ("f",P.epsilon) in
+  let states = States.singleton q in
+  let r1 = (q, mkCtor zero, P.epsilon, mk_drain) in
+  let r2 = (q, mkCtor succ, P.epsilon, mk_states [q]) in
+  let rules = RuleSet.of_list [r1;r2] in
+    create sigma states rules q
+
 let test_accept_zero_ok () =
   let term = mkCtor zero in
     assert_bool "Zero CARTA should accept term" @@ BatResult.is_ok @@ accepts carta_zero term
@@ -35,10 +46,15 @@ let test_accept_nil_bad () =
   let term = mkCtor nil in
     assert_bool "Zero CARTA must not accept term" @@ BatResult.is_bad @@ accepts carta_zero term
 
+let test_accept_zero_num_ok () =
+  let term = mkCtor zero in
+    assert_bool "Number CARTA should accept term zero" @@ BatResult.is_ok @@ accepts carta_zero term
+
 let init_tests () =
   [
    ("accept zero ok", test_accept_zero_ok);
    ("accept nil bad", test_accept_nil_bad);
+   ("accept zero num ok", test_accept_zero_num_ok);
   ]
 
 let _ = install_tests_new "accept" init_tests
